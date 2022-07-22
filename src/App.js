cd97a34b43f0b06e724cafa664001b05a637f0e0
abir14016 <abir14016@gmail.com>
2022-07-22 09:39:03 +0600

    demo footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Header></Header>
       <button className="btn btn-primary">Button</button>
       <button className="btn btn-secondary">DaisyUi</button>
+      <Footer></Footer>
     </div>
   );
 }
