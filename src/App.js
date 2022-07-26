9e5cb7e21424403c584429932dfd9e6191a90da7
abir14016 <abir14016@gmail.com>
2022-07-26 06:04:00 +0600

    menubar initial setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Dashboard from './Pages/Dashboard/Dashboard/Dashboard';
 import Home from './Pages/Home/Home/Home';
 import SignIn from './Pages/Login/SignIn/SignIn';
 import SignUp from './Pages/Login/SignUp/SignUp';
+import Search from './Pages/Search/Search/Search';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -15,6 +19,10 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
         <Route path='/signin' element={<SignIn></SignIn>}></Route>
+        <Route path='/search' element={<Search></Search>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,27 +1,56 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <header className='bg-base-200'>
+        <header class="navbar bg-base-200">
             <div className="navbar md:container md:mx-auto">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex="0" className="btn btn-ghost btn-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                <div class="navbar-start">
+                    <div class="dropdown">
+                        <label tabindex="0" class="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Homepage</a></li>
-                            <li><a>Portfolio</a></li>
-                            <li><a>About</a></li>
+                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><Link to="/">Home</Link></li>
+                            <li tabindex="0">
+                                <a>
+                                    Pets
+                                    <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                                </a>
+                                <ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                    <li><a>Submenu 1</a></li>
+                                    <li><a>Submenu 2</a></li>
+                                </ul>
+                            </li>
+                            <li><Link to="/about">About</Link></li>
                         </ul>
                     </div>
-                </div>
-                <div className="navbar-center">
-                    <a className="btn btn-ghost normal-case text-xl">
+                    <Link to="/" className="btn btn-ghost normal-case text-xl">
                         PET<span className='text-secondary font-extrabold'>BAZAR</span>
-                    </a>
+                    </Link>
+                </div>
+                <div class="navbar-center hidden lg:flex">
+                    <ul class="menu menu-horizontal p-0">
+                        <li><Link to="/">Home</Link></li>
+                        <li tabindex="0">
+                            <a>
+                                Pets
+                                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                            </a>
+                            <ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><Link to="/about">about</Link></li>
+                    </ul>
                 </div>
                 <div className="navbar-end">
+                    <Link to="/search">
+                        <button class="btn btn-ghost btn-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </Link>
                     <div className="dropdown dropdown-end">
                         <label tabIndex="0" className="btn btn-ghost btn-circle">
                             <button className="btn btn-ghost btn-circle">
@@ -56,6 +85,7 @@ const Header = () => {
                             </li>
                             <li><a>Settings</a></li>
                             <li><a>Logout</a></li>
+                            <li><Link to="/dashboard">Dashboard</Link></li>
                         </ul>
                     </div>
                 </div>
